aaf2830eaa7cdb1035461b79aedf38dade918d26
Kirk Tautz <kirktautz@Kirks-MacBook-Pro.local>
2017-10-31 11:25:02 -0600

    started authservice singleton

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,19 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ success: Bool) -> ()
+
 // Segues
 let TO_LOGIN = "toLogin"
 let TO_SIGN_UP = "toSignUp"
 let UNWIND_TO_CHANNEL = "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+// URL Constants
+
+let BASE_URL = "https://chat-app-dev-kirk.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
